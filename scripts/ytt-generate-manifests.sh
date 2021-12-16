bfdb58c6f88b9837e5d483da58f92a67402de75f
Armen Rostomovich <me@rmnr.net>
2021-12-16 13:47:25 -0800

    meh - I'm a terrible person on these commits

--- scripts/ytt-generate-manifests.sh
+++ scripts/ytt-generate-manifests.sh
@@ -17,7 +17,9 @@ while IFS= read -r -d '' app_directory ; do
 
   # render Helm templates if Chart.yaml file is found
   SYNCED_DIR="./deploy/synced/$app_name"
+  
   if [ -f "./deploy/synced/$app_name/Chart.yaml" ]; then
+  
   printf "\n🚨 🛎  📢  Found Helm Chart for $app_name !! \n"
     tmp_helm_rendered=$(mktemp -u).yml
     helm template "$app_name" "./deploy/synced/$app_name" > "$tmp_helm_rendered"
@@ -26,8 +28,10 @@ while IFS= read -r -d '' app_directory ; do
   fi
 
   if [ -d "./deploy/overlays/$app_name" ]; then
+  
   printf "\n🛠  🧙 🪄   Magically generated --> $app_name/deploy.yaml \n\n"
   ytt --file "$SYNCED_DIR" --file "./deploy/overlays/$app_name" > "./deploy/generated-manifests/$app_name/deploy.yaml"
+  
   fi
 
 done < <(find ./deploy/synced/* -maxdepth 0 -type d -print0)
